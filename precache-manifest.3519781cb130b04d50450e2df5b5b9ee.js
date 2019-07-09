fcb5b78ec06c49458f01cc8e8d61a352ec0cecc3
Yukichka <yukiinsw@gmail.com>
2019-07-09 23:45:53 +0100

    Updates

---
+++ precache-manifest.3519781cb130b04d50450e2df5b5b9ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfa2078cccd84c1a72389a60866a7452",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc288eb07e4b8c2ca1cd",
+    "url": "/static/css/2.5d70476f.chunk.css"
+  },
+  {
+    "revision": "016b66c96693917560ed",
+    "url": "/static/css/main.6bf81c95.chunk.css"
+  },
+  {
+    "revision": "bc288eb07e4b8c2ca1cd",
+    "url": "/static/js/2.0f6f94cc.chunk.js"
+  },
+  {
+    "revision": "016b66c96693917560ed",
+    "url": "/static/js/main.13f31454.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
