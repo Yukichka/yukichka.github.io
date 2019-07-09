4c519b3937b518132e7123083bdde685df3717b8
Yukichka <yukiinsw@gmail.com>
2019-07-09 12:21:46 +0100

    Updates

---
+++ precache-manifest.5b961fcf6a66e42417eb86107189c529.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cfe634bcd48b1bff4e8a",
+    "url": "/static/js/main.2f86cdff.chunk.js"
+  },
+  {
+    "revision": "0b8d03fddd1a56b05130",
+    "url": "/static/js/2.51a1971d.chunk.js"
+  },
+  {
+    "revision": "cfe634bcd48b1bff4e8a",
+    "url": "/static/css/main.44003eaa.chunk.css"
+  },
+  {
+    "revision": "0b8d03fddd1a56b05130",
+    "url": "/static/css/2.b6a70ca4.chunk.css"
+  },
+  {
+    "revision": "0a631e0bf53a2e15ec200691075462ba",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ebbf75220ff89dacdcb8c4772884eb5.js"
+  "/precache-manifest.5b961fcf6a66e42417eb86107189c529.js"
 );
 
 workbox.clientsClaim();
